09575fb0bda0b39fb86aacbb64b2884c62982283
Tanmay3005 <51704952+Tanmay3005@users.noreply.github.com>
2022-02-07 09:55:28 +0530

    Update main_body.py

--- truth_and_dare/main_body.py
+++ truth_and_dare/main_body.py
@@ -6,12 +6,11 @@ we hope you enjoy
 
 ragards
 Leaf Green'''
-name=str(input("sir can we know ur name !!!")
- print("hello",name,"the rules are simple you just have to tell 3 or 2 or 1 numbers simultaneously,you will be competing against computer,if you or computer got to speak the number 21,he have to choose between truth or dare and what the user and computer says")
+name=(input("sir can we know ur name !!!"))
+print("hello",name,"the rules are simple you just have to tell 3 or 2 or 1 numbers simultaneously,you will be competing against computer,if you or computer got to speak the number 21,he have to choose between truth or dare and what the user and computer says")
 print("so lets begin")
-start=input("so lets begin")
-         if start==yes or Yes or YES:
-            import starting
-         else:
+start=input("do you want to start(y,n)")
+if start=='yes' or 'Yes' or 'YES' or 'Y' or 'y' :
+  import starting
+else:
           print("bye,see you")
-      

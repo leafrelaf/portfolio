9e985ab8dc0b4ad545ece8b0d974d168874247d4
Tanmay3005 <51704952+Tanmay3005@users.noreply.github.com>
2022-02-07 09:36:05 +0530

    Create main_body.py

---
+++ truth_and_dare/main_body.py
@@ -0,0 +1,10 @@
+'''hello sir
+we hope you love playing truth and dare
+we hope you revel us ur all secrets( just kidding)
+we hope you enjoy
+
+
+ragards
+Leaf Green'''
+name=str(input("sir can we know ur name !!!")
+

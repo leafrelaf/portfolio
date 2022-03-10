35a99aa6211e2a5eeaad087c4ae8e6846f135c87
leaf_relaf <51704952+leafrelaf@users.noreply.github.com>
2022-03-10 11:31:37 +0530

    Create prime number.py

---
+++ find prime numbers/prime number.py
@@ -0,0 +1,8 @@
+'''to check if a number is prime or not'''
+x=int(input("enter a number"))
+for i in range (2,x):
+   if (x%i)==0:
+     print(f"{x} number is not prime")
+     break
+else:
+      print(f"{x}number is prime")

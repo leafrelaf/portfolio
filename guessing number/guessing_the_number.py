96758e06a0c5b577238adbbbca6d11447eb1f6a5
leaf_relaf <51704952+leafrelaf@users.noreply.github.com>
2022-03-11 19:34:02 +0530

    Update guessing_the_number.py

--- guessing number/guessing_the_number.py
+++ guessing number/guessing_the_number.py
@@ -12,39 +12,49 @@ guessing_number=randint(1,100)
 print("the number have been choosen")
 sleep(1)
 if guessing_number%2==0:
-    print("the guessed number is even")
+    print("hint 1:the guessed number is even")
 elif guessing_number%2==1:
- print("the guessed number is odd")
+ print("hint 1:the guessed number is odd")
 A=guessing_number-5 
 B=guessing_number+5
 
-print("hint:the number is between",A,"&",B)
+print("hint 2:the number is between",A,"&",B)
+if A<0:
+   print("number is positive")
 
-print("ok")
+
 if guessing_number%3==0:
-    print("the number is devisible by 3")
+    print("hint 3 :the number is devisible by 3")
 elif guessing_number%4==0:
-   print("the number is devisibe by 4")
+   print("hint 3 :the number is devisibe by 4")
 elif  guessing_number%5==0:
-    print("the number is devisible by 5")
+    print("hint 3 :the number is devisible by 5")
 elif  guessing_number%6==0:
-   print("the number is devisible by 6")
+   print("hint 3 :the number is devisible by 6")
 elif  guessing_number%7==0:
-   print("the number is devisible by 7")
+   print("hint 3 :the number is devisible by 7")
 elif  guessing_number%8==0:
-   print("the number is devisible by 8")
+   print("hint 3 :the number is devisible by 8")
 elif  guessing_number%9==0:
-   print("the number is devisible by 9")
+   print("hint 3 :the number is devisible by 9")
 elif  guessing_number%10==0:
-   print("the number is devisible by 10")
+   print("hint 3 :the number is devisible by 10")
 else:
-  print("number is a prime number") 
+  print("hint 3 :number is a prime number") 
 end=int(input("so what do u think number can be ?"))
 if end==guessing_number:
    print("u won")
+   print("the answer was",guessing_number)
 else:
-   print("sup")
+   print("u r wrong")
+   print("the answer was",guessing_number)
 
+
+again=input("do you want to play again ?(Y/N)")
+if again=="Y" or "y":
+   import number_guesser
+elif again=="N" or "n":
+   quit()
 
     
 
